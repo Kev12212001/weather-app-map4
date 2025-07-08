d890406435f2fedd60ab1b1a1555b030a089c1f7
Kev12212001 <kevin.oconnell9549@gmail.com>
2025-07-08 18:01:14 -0500

    Update app5.py

--- app5.py
+++ app5.py
@@ -15,7 +15,7 @@ from scipy.interpolate import griddata  # For interpolation
 from datetime import datetime
 import pytz
 
-app5 = Flask(__name__)
+app = Flask(__name__)
 
 # Your API key for the weather data
 API_KEY = '6e1f7ca9bda347cdb5a5b9259a0bafc9'
@@ -89,9 +89,9 @@ locations = {
     # Add more locations...
 }
 
-@app5.route('/')
+@app.route('/')
 
-@app5.route('/weather-map')
+@app.route('/weather-map')
 def map5_view():
     # Step 1: Concurrently fetch weather data for all locations
     with ThreadPoolExecutor() as executor:
@@ -413,4 +413,4 @@ def fetch_weather_data(city, coords):
     return None
 
 if __name__ == '__main__':
-    app5.run(debug=True)
+    app.run(debug=True)
